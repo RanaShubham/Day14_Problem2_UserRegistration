74ca289ab4f46c6435445d7be049173b4b062fe7
Shubham Rana <ranashubham7296@gmail.com>
2020-08-14 15:05:56 +0530

    Added pattern to verify mobile number

--- userRegister.sh
+++ userRegister.sh
@@ -13,3 +13,25 @@ then
 else
 	echo "Invalid name"
 fi
+
+read -p "Enter a valid email ID: " email
+
+ePattern="^[[:lower:]]{1,}([.][[:lower:]]{1,}){0,}@[[:lower:]]{2,}[.][[:lower:]]{2,4}([.][[:lower:]]{2,}){0,1}$"
+
+if [[ $email =~ $ePattern ]]
+then
+	echo "Valid email"
+else
+	echo "Invaid email"
+fi
+
+read -p "Enter mobile number: " MobileNumber
+
+mPattern="^[91]{0,}[7-9]{1}[0-9]{9}$"
+
+if [[ $MobileNumber =~ $mPattern  ]]
+then
+	echo "Valid mobile number"
+else
+	echo "Invalid mobile number"
+fi
