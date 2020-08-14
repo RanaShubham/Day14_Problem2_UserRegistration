e59d99e20fdd23920fccf286f949225c39d9d88c
Shubham Rana <ranashubham7296@gmail.com>
2020-08-14 13:51:18 +0530

    Adding pattern to verify first name

--- userRegister.sh
+++ userRegister.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Welcome to user verification program"
+
+read -p "Enter a valid first name: " FirstName
+
+FnPattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+if [[ $FirstName =~ $FnPattern ]]
+then
+	echo "Valid name"
+else
+	echo "Invalid name"
+fi
